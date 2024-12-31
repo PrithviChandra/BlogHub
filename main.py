b5f8bec93f1a3f489444191025e16adb182e039f
PrithviChandra <prithvichandra.mec@gmail.com>
2024-12-30 19:50:06 -0600

    Pushing latest changes for deployment

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 import smtplib
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -248,8 +249,8 @@ def contact():
         try:
             smtp_server = "smtp.gmail.com"
             smtp_port = 587
-            sender_email = "prithvi.bchandran@gmail.com"
-            sender_password = "bzfcaoisxmpgipnm"
+            sender_email = os.environ.get('SENDER_EMAIL')
+            sender_password = os.environ.get('SENDER_PASSWORD')
             with smtplib.SMTP(smtp_server, smtp_port) as server:
                 server.starttls()
                 server.login(sender_email,sender_password)
@@ -264,4 +265,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
